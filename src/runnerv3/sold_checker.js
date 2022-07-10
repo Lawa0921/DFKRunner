07f7c3603e6d8259c377e06bf2245e9ba298fafd
Galen Lin <bag571ivy3470@gmail.com>
2022-07-10 23:16:42 +0800

    feat(script): add sold_checker script

---
+++ src/runnerv3/sold_checker.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const config = require("./config.json");
+const autils = require("./autils")
+const axios = require('axios')
+const axiosRetry = require('axios-retry');
+
+axiosRetry(axios, {
+  retries: 5, // number of retries
+  retryDelay: (retryCount) => {
+    console.log(`retry attempt: ${retryCount}`);
+    return retryCount * 2000; // time interval between retries
+  },
+  retryCondition: (error) => {
+    return error.response.status === 500;
+  },
+});
+
+const isAPIv6Owner = async (heroID) => {
+  let returnValue;
+
+  await axios.post(config.queryHeroEndPoint, {"limit":1,"params":[{"field":"id","operator":"=","value":heroID.toString()}],"offset":0}).then((res) => {
+    if (res.data[0].owner_address.toLowerCase() === process.env.WALLET_ADDRESS.toLowerCase()) {
+      returnValue = true;
+    }
+  }).catch((_err) => {
+    returnValue = false;
+  })
+  return returnValue;
+}
+
+async function main() {
+  const onSaleHeroIds = config.heroForSale.map((heroData) => { return heroData.id });
+
+  for (let index = 0; onSaleHeroIds.length > index; index++) {
+    if (isAPIv6Owner(onSaleHeroIds[index])) {
+      console.log(`Holding hero ${onSaleHeroIds[index]}`)
+    } else {
+      console.log(`Hero ${onSaleHeroIds[index]} is sold!`)
+    }
+  }
+}
+
+main()
