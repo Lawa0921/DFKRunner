e096627f31fc2b0989244e9b5576b00e434d7838
Galen Lin <bag571ivy3470@gmail.com>
2022-07-11 15:19:28 +0800

    chore(log): more readable sale_watcher output

--- src/runnerv3/autils.js
+++ src/runnerv3/autils.js
@@ -61,11 +61,6 @@ exports.rewardLog = (myString) => {
     fs.appendFileSync("rewards", myString + "\n");
 }
 
-exports.watchHeroLog = (hero, price, valuator) => {
-    str = `${new Date().toLocaleTimeString()} id: ${hero.id}, ${hero.formatRarity()} LV${hero.level} ${hero.profession} G${hero.generation} ${hero.summons_remaining}/${hero.maxsummons} ${hero.formatMainclass()}/${hero.formatSubclass()}, ${hero.statboost1} ${hero.statboost2} listed ${parseInt(price) / Math.pow(10, 18)} J, valuate: ${valuator} J `
-    console.log(str);
-}
-
 exports.bidHeroLog = (message) => {
     console.log(message);
     fs.appendFileSync("bid_heros", message + "\n");
@@ -83,4 +78,88 @@ exports.txnFailLog = (txn) => {
 
 exports.gasSettingFormater = () => {
     return { gasPrice: config.gasPrice, gasLimit: config.gasLimit };
+}
+
+exports.watchHeroLog = async (hero, price, valuator) => {
+    let idAndRarity = `${hero.id} ${hero.formatRarity()}`;
+    let info = `\x1b[3m LV${hero.level} ${hero.profession} G${hero.generation} ${hero.summons_remaining}/${hero.maxsummons} \x1b[0m`;
+    let mainClass = hero.formatMainclass();
+    let subClass =  hero.formatSubclass();
+    let skillInfo = `skill: ${heroSkillDyer(hero, "active1")}/${heroSkillDyer(hero, "active2")}/${heroSkillDyer(hero, "passive1")}/${heroSkillDyer(hero, "passive2")}`
+    let listInfo = `listed ${strToAnsiRed(parseInt(price) / Math.pow(10, 18))} J, valuate: ${strToAnsiRed(valuator)} J`
+
+    if (hero.formatRarity()=== "UnCommon") {
+        idAndRarity = strToAnsiGreen(idAndRarity);
+    } else if (hero.formatRarity() === "Rare") {
+        idAndRarity = strToAnsiBlue(idAndRarity);
+    } else if (hero.formatRarity() === "Legendary") {
+        idAndRarity = strToAnsiOrange(idAndRarity);
+    } else if (hero.formatRarity() === "Mythic") {
+        idAndRarity = strToAnsiPurple(idAndRarity);
+    }
+
+    if (hero.attributeTier("mainclass") === "Advanced") {
+        mainClass = strToAnsiGreen(mainClass);
+    } else if (hero.attributeTier("mainclass") === "Elite") {
+        mainClass = strToAnsiBrown(mainClass);
+    } else if (hero.attributeTier("mainclass") === "Transcendant") {
+        mainClass = strToAnsiRed(mainClass);
+    }
+
+    if (hero.attributeTier("subclass") === "Advanced") {
+        subClass = strToAnsiGreen(subClass);
+    } else if (hero.attributeTier("subclass") === "Elite") {
+        subClass = strToAnsiBrown(subClass);
+    } else if (hero.attributeTier("subclass") === "Transcendant") {
+        subClass = strToAnsiRed(subClass);
+    }
+
+    str = idAndRarity + ", " + mainClass + "/" + subClass + " " + info + " +" + strToAnsiGreen(hero.statboost1) + "/" + strToAnsiBlue(hero.statboost2) + " " + skillInfo + " " + listInfo
+    console.log(str);
+}
+
+const strToAnsiRed = (str) => {
+    return "\x1b[31m" + str + "\x1b[0m"
+}
+
+const strToAnsiGreen = (str) => {
+    return "\x1b[32m" + str + "\x1b[0m"
+}
+
+const strToAnsiOrange = (str) => {
+    return "\x1b[33m" + str + "\x1b[0m"
+}
+
+const strToAnsiBlue = (str) => {
+    return "\x1b[34m" + str + "\x1b[0m"
+}
+
+const strToAnsiPurple = (str) => {
+    return "\x1b[35m" + str + "\x1b[0m"
+}
+
+const strToAnsiCyan = (str) => {
+    return "\x1b[36m" + str + "\x1b[0m"
+}
+
+const strToAnsiBrown = (str) => {
+    return "\x1b[93m" + str + "\x1b[0m"
+}
+
+const heroSkillDyer = (hero, attributeName) => {
+    let skill = hero[attributeName];
+    let attributeTier = hero.attributeTier(attributeName);
+    let returnValue;
+
+    returnValue = skill[0] + skill[skill.length - 1];
+
+    if (attributeTier === "Advanced") {
+        returnValue = strToAnsiGreen(returnValue)
+    } else if (attributeTier === "Elite") {
+        returnValue = strToAnsiCyan(returnValue)
+    } else if (attributeTier === "Transcendant") {
+        returnValue = strToAnsiPurple(returnValue)
+    }
+
+    return returnValue;
 }
