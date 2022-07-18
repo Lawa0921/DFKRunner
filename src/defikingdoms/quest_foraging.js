756c1016ce08a2f2a8829cc7daadc1a5a2a818a9
Galen Lin <bag571ivy3470@gmail.com>
2022-07-18 21:55:42 +0800

    feat(script): add dfk chain send foraging quest logic

--- src/defikingdoms/quest_foraging.js
+++ src/defikingdoms/quest_foraging.js
@@ -1,39 +1,39 @@
 const config = require("~/config.js");
 const QuestCoreV2 = require('~/src/defikingdoms/contracts/questCoreV2')
 
-exports.CheckAndSendDFKFishers = async (heroesStruct, isPro) => {
-  const questType = config.defikingdoms.quest.fishing
+exports.CheckAndSendDFKForagers = async (heroesStruct, isPro) => {
+  const questType = config.defikingdoms.quest.foraging
   let minBatch = isPro ? questType.professionHeroes.length : questType.nonProfessionHeroes.length;
 	let maxBatch = 1;
 	minBatch = minBatch > maxBatch ? maxBatch : minBatch;
 	let proStamUsage = 5;
 	let normStamUsage = 7;
 	let minStam = isPro ? questType.proMinStam : questType.normMinStam;
-	let proFishingTries = Math.floor(minStam/proStamUsage);
-	let normFishingTries = Math.floor(minStam/normStamUsage);
-	let fishingTries = isPro ? proFishingTries : normFishingTries;
+	let proForagingTries = Math.floor(minStam/proStamUsage);
+	let normForagingTries = Math.floor(minStam/normStamUsage);
+	let foragingTries = isPro ? proForagingTries : normForagingTries;
 
 	let activeQuesters = heroesStruct.allQuesters
-	let configFishers = isPro ? questType.professionHeroes : questType.nonProfessionHeroes
-	let possibleFishers = configFishers.filter((e) => {
+	let configForagers = isPro ? questType.professionHeroes : questType.nonProfessionHeroes
+	let possibleForagers = configForagers.filter((e) => {
 		return (activeQuesters.indexOf(e) < 0);
 	});
 
-  let FisherPromises = []
+  let ForagerPromises = []
 
-	for (let i = 0; i < possibleFishers.length; i++ ) {
-		FisherPromises.push(await new QuestCoreV2("dfk").getCurrentStamina(possibleFishers[i]))
+	for (let i = 0; i < possibleForagers.length; i++ ) {
+		ForagerPromises.push(await new QuestCoreV2("dfk").getCurrentStamina(possibleForagers[i]))
 	}
 
 
-	let staminaValues = await Promise.allSettled(FisherPromises);
+	let staminaValues = await Promise.allSettled(ForagerPromises);
 	staminaValues = staminaValues.map(res => res = res.value?.toNumber() || 0);
 	
 	LocalBatching = []
-	for (let index = 0; index < possibleFishers.length; index++) {
+	for (let index = 0; index < possibleForagers.length; index++) {
 		const stam = staminaValues[index];
 		if ( stam >= minStam ) {
-			LocalBatching.push(possibleFishers[index]);
+			LocalBatching.push(possibleForagers[index]);
 		}
 
 		if (LocalBatching.length === maxBatch) {
@@ -42,8 +42,8 @@ exports.CheckAndSendDFKFishers = async (heroesStruct, isPro) => {
 	}
 
   if (LocalBatching.length > 0) {
-    console.log("senting " + LocalBatching + " to fishing quest");
-    const tx = await new QuestCoreV2("dfk").startFishingQuest(LocalBatching, fishingTries)
+    console.log("senting " + LocalBatching + " to foraging quest");
+    const tx = await new QuestCoreV2("dfk").startForagingQuest(LocalBatching, foragingTries)
 		let res = await tx.wait();
 
 		if (res.status === 1) {
@@ -52,6 +52,6 @@ exports.CheckAndSendDFKFishers = async (heroesStruct, isPro) => {
 			autils.txnFailLog("sent " + LocalBatching + " failed");
 		}
   } else {
-		console.log("No Fisher sent")
+		console.log("No Forager sent")
 	} 
 }
--- src/defikingdoms/quest_runner.js
+++ src/defikingdoms/quest_runner.js
@@ -1,6 +1,7 @@
 const dataParser = require('~/src/services/data_parser') 
 const QuestCoreV2 = require("~/src/defikingdoms/contracts/questCoreV2");
-const { CheckAndSendDFKFishers } = require("~/src/defikingdoms/quest_fishing")
+const { CheckAndSendDFKFishers } = require("~/src/defikingdoms/quest_fishing");
+const { CheckAndSendDFKForagers } = require("~/src/defikingdoms/quest_foraging");
 const config = require('~/config.js')
 const autils = require("~/src/services/autils")
 const questCoreV2Contract = new QuestCoreV2("dfk")
@@ -12,6 +13,7 @@ exports.runDFKChainQuest = async () => {
     const heroesStruct = await dataParser.heroDataParse(activeQuests);
 
     await CheckAndSendDFKFishers(heroesStruct, true);
+    await CheckAndSendDFKForagers(heroesStruct, true);
 
     console.log("DFK Chain quest process completed");
   } catch(error) {
