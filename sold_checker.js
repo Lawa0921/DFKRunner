cbaf515a38da5a7b809beac3710c0f05f215a411
Galen Lin <bag571ivy3470@gmail.com>
2022-07-20 14:59:46 +0800

    feat(script): check dfk sold feature

--- sold_checker.js
+++ sold_checker.js
@@ -2,7 +2,9 @@ const config = require("~/config.js");
 const autils = require('~/src/services/autils');
 
 async function main() {
-  const onSaleHeroIds = config.harmony.heroForSale.map((heroData) => { return heroData.id });
+  const harmonyOnSaleHeroIds = config.harmony.heroForSale.map((heroData) => { return heroData.id });
+  const dfkOnSaleHeroIds = config.defikingdoms.heroForSale.map((heroData) => { return heroData.id });
+  const onSaleHeroIds = harmonyOnSaleHeroIds.concat(dfkOnSaleHeroIds);
 
   for (let index = 0; onSaleHeroIds.length > index; index++) {
     if (await autils.isAPIv6Owner(onSaleHeroIds[index])) {
