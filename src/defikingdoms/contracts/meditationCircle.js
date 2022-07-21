13639b8295d0d4c9a3231a0b570423dfbfcf3ea1
Galen Lin <bag571ivy3470@gmail.com>
2022-07-21 23:59:34 +0800

    chore(contract): add MeditationCircle contract logic

---
+++ src/defikingdoms/contracts/meditationCircle.js
@@ -0,0 +1,38 @@
+const config = require("~/config.js");
+const ethers = require('ethers');
+const meditationCircleABI = require('~/abis/MeditationCircle.json')
+
+module.exports = class MeditationCircle {
+  constructor() {
+    this.provider = new ethers.providers.JsonRpcProvider(config.defikingdoms.rpcs[config.defikingdoms.useRpcIndex])
+    this.wallet = new ethers.Wallet(config.privateKey, this.provider)
+    this.contract = new ethers.Contract(config.defikingdoms.meditationCircle, meditationCircleABI, this.wallet)
+  }
+
+  async startMeditation(heroId, mainStat, subStat1, subStat2) {
+    return await this.contract.startMeditation(heroId, this.getStatInt(mainStat), this.getStatInt(subStat1), this.getStatInt(subStat2), autils.get0xAddress());
+  }
+
+  async completeMeditation(heroId) {
+    return await this.contract.completeMeditation(heroId);
+  }
+
+  async getActiveMeditations() {
+    return await this.contract.getActiveMeditations(config.walletAddress);
+  }
+
+  getStatInt(stat) {
+    const stats = {
+      'STR': 0,
+      'AGI': 1,
+      'INT': 2,
+      'WIS': 3,
+      'LCK': 4,
+      'VIT': 5,
+      'END': 6,
+      'DEX': 7
+    }
+
+    return stats[stat];
+  }
+}
---
+++ src/harmony/contracts/meditationCircle.js
@@ -0,0 +1,50 @@
+const config = require("~/config.js");
+const autils = require("~/src/services/autils");
+const meditationCircleABI = require('~/abis/MeditationCircle.json');
+const { Harmony } = require('@harmony-js/core');
+const {
+    ChainID,
+    ChainType,
+  } = require('@harmony-js/utils');
+
+const hmy = new Harmony(
+  autils.getRpc(config.harmony.useRpcIndex), {
+    chainType: ChainType.Harmony,
+    chainId: ChainID.HmyMainnet,
+  }
+);
+
+hmy.wallet.addByPrivateKey(config.privateKey);
+
+module.exports = class MeditationCircle {
+  constructor() {
+    this.contract = hmy.contracts.createContract(meditationCircleABI, config.harmony.meditationCircle)
+  }
+
+  async startMeditation(heroId, mainStat, subStat1, subStat2) {
+    return await this.contract.methods.startMeditation(heroId, this.getStatInt(mainStat), this.getStatInt(subStat1), this.getStatInt(subStat2), autils.get0xAddress()).send(autils.gasSettingFormater());
+  }
+
+  async completeMeditation(heroId) {
+    return await this.contract.methods.completeMeditation(heroId).send(autils.gasSettingFormater());
+  }
+
+  async getActiveMeditations() {
+    return await this.contract.methods.getActiveMeditations(config.walletAddress).call();
+  }
+
+  getStatInt(stat) {
+    const stats = {
+      'STR': 0,
+      'AGI': 1,
+      'INT': 2,
+      'WIS': 3,
+      'LCK': 4,
+      'VIT': 5,
+      'END': 6,
+      'DEX': 7
+    }
+
+    return stats[stat];
+  }
+}
