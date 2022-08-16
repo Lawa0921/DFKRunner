74cae68036fff53fcb931a321acc4c0f8bc7713f
Galen Lin <bag571ivy3470@gmail.com>
2022-08-16 23:48:11 +0800

    bug(autils): fix getHarmonyOwningHeroIds function bug

--- src/services/autils.js
+++ src/services/autils.js
@@ -243,8 +243,8 @@ exports.getHarmonyOwningHeroIds = () => {
     heroIds = heroIds.concat(questConfig.gardening.professionHeroes.map((heroData) => { return heroData.heroID }));
     heroIds = heroIds.concat(questConfig.gardening.nonProfessionHeroes.map((heroData) => { return heroData.heroID }));
   
-    for (let i = 0; i < config.harmony.statQuests.length; i++ ) {
-      heroIds = heroIds.concat(config.harmony.statQuests[i].heroes);
+    for (let i = 0; i < config.harmony.quest.statQuests.length; i++ ) {
+      heroIds = heroIds.concat(config.harmony.quest.statQuests[i].heroes);
     }
 
     return heroIds;
