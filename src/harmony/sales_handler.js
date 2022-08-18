c3004cb92b822dd3a15b816530ea011edb8d248f
Galen Lin <bag571ivy3470@gmail.com>
2022-08-18 16:15:02 +0800

    chore(script): update harmony chain sale_hander logic

--- src/harmony/sales_handler.js
+++ src/harmony/sales_handler.js
@@ -1,51 +1,29 @@
 const config = require("~/config.js");
-const autils = require('~/src/services/autils')
-const QuestCoreV2 = require('~/src/harmony/contracts/questCoreV2');
-const questCoreV2Contract = new QuestCoreV2();
-const HeroCore = require('~/src/harmony/contracts/heroCore');
-let heroCoreContract = new HeroCore();
-const SaleAuction = require('~/src/harmony/contracts/saleAuction');
-let saleAuctionContract = new SaleAuction();
+const autils = require('~/src/services/autils');
+const SaleAuction = require("~/src/harmony/contracts/saleAuction");
+const saleAuctionContract = new SaleAuction();
 
-const isHeroOnSale = (ownerAddress) => {
-    return ownerAddress.toLowerCase() === config.harmony.saleAuction.toLowerCase();
+const isOwning = (heroObject) => {
+  return heroObject.owner === config.walletAddress;
 }
 
-const isOwning = (ownerAddress) => {
-    return ownerAddress.toLowerCase() === config.walletAddress.toLowerCase();
+const isShouldList = (heroObject) => {
+  return isOwning(heroObject) && !heroObject.isOnSale && !heroObject.isOnRent && heroObject.currentStamina < config.defikingdoms.listStamina
 }
 
-const isShouldList = (ownerAddress, stamina) => {
-    return isOwning(ownerAddress) && !isHeroOnSale(ownerAddress) && stamina < config.harmony.listStamina && stamina !== -1;
-}
-
-const isShouldUnList = async (ownerAddress, stamina, heroId) => {
-    return isHeroOnSale(ownerAddress) && stamina > config.harmony.unlistStamina && await autils.isAPIv6Owner(heroId);
+const isShouldUnList = (heroObject) => {
+  return heroObject.isOnSale && heroObject.currentStamina > config.defikingdoms.unlistStamina;
 }
 
 exports.runSalesLogic = async () => {
-    const heroList = config.harmony.heroForSale;
-    let staminaValues = [];
-	for ( let i = 0; i < heroList.length; i++ ) {
-		staminaValues.push(questCoreV2Contract.getCurrentStamina(heroList[i].id));
-	}
+  const heroList = config.harmony.heroForSale.sort((a, b) => { return parseInt(a.id) - parseInt(b.id); });
+  const heroObjects = await autils.getHerosInfo(heroList.map((heroData) => { return heroData.id }))
 
-	staminaValues = await Promise.all(staminaValues);
-
-    let heroOwners = [];
-    for (let i = 0; i < heroList.length; i++ ) {
-        heroOwners.push(heroCoreContract.ownerOf(heroList[i].id));
+  for (let i = 0; i < heroObjects.length; i++) {
+    if (isShouldUnList(heroObjects[i])) {
+      await saleAuctionContract.unlistHero(heroList[i].id);
+    } else if (isShouldList(heroObjects[i])) {
+      await saleAuctionContract.listHero(heroList[i].id, heroList[i].price);
     }
-
-    heroOwners = await Promise.all(heroOwners);
-
-    for (let i = 0; i < heroOwners.length; i++) {
-        if (await isShouldUnList(heroOwners[i], staminaValues[i], heroList[i].id)) {
-            await saleAuctionContract.unlistHero(heroList[i].id);
-        } else if (isShouldList(heroOwners[i], staminaValues[i])) {
-            await saleAuctionContract.listHero(heroList[i].id, heroList[i].price);
-        }
-    }
-
-    return;
+  }
 }
