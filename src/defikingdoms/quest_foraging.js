83ea8b1f653e5d7f5b025740b4ffc185b303b3d9
Galen Lin <bag571ivy3470@gmail.com>
2022-08-20 00:02:48 +0800

    chore(script): adjust 2F quest logic

--- src/defikingdoms/quest_foraging.js
+++ src/defikingdoms/quest_foraging.js
@@ -3,20 +3,25 @@ const autils = require('~/src/services/autils');
 const QuestCoreV2 = require('~/src/defikingdoms/contracts/questCoreV2');
 const questCoreV2Contract = new QuestCoreV2();
 const minStamina = 25;
+const maxQueue = 6;
 
 exports.CheckAndSendDFKForagers = async (heroesStruct) => {
-	const questType = config.defikingdoms.quest.foraging
-	const activeQuesterIds = heroesStruct.allQuesters
-	const heroObjects = await autils.getHerosInfo(questType.heroes)
-	const possibleForagers = heroObjects.filter((heroObject) => { return activeQuesterIds.indexOf(heroObject.id) === -1 && heroObject.currentStamina >= minStamina && heroObject.owner === config.walletAddress })
-
-	if (possibleForagers.length > 0) {
-		for (let i = 0; i < possibleForagers.length; i++) {
-			console.log(`senting ${possibleForagers[i].id} to foraging quest`);
-			const attemp = possibleForagers[i].profession === "foraging" ? Math.floor(possibleForagers[i].currentStamina / 5) : Math.floor(possibleForagers[i].currentStamina / 7)
-			await questCoreV2Contract.startForagingQuest([possibleForagers[i].id], attemp)
-		}
+	if (heroesStruct.foragingQuestCount >= maxQueue) {
+		console.log("foraging queue has reached its maximum value.")
 	} else {
-		console.log("No Forager Sent")
+		const questType = config.defikingdoms.quest.foraging
+		const activeQuesterIds = heroesStruct.allQuesters
+		const heroObjects = await autils.getHerosInfo(questType.heroes)
+		const possibleForagers = heroObjects.filter((heroObject) => { return activeQuesterIds.indexOf(heroObject.id) === -1 && heroObject.currentStamina >= minStamina && heroObject.owner === config.walletAddress })
+
+		if (possibleForagers.length > 0) {
+			for (let i = 0; i < maxQueue - heroesStruct.foragingQuestCount - 1; i++) {
+				console.log(`senting ${possibleForagers[i].id} to foraging quest`);
+				const attemp = possibleForagers[i].profession === "foraging" ? Math.floor(possibleForagers[i].currentStamina / 5) : Math.floor(possibleForagers[i].currentStamina / 7)
+				await questCoreV2Contract.startForagingQuest([possibleForagers[i].id], attemp);
+			}
+		} else {
+			console.log("No Forager Sent")
+		}
 	}
 }
--- src/harmony/quest_fishing.js
+++ src/harmony/quest_fishing.js
@@ -3,20 +3,25 @@ const autils = require('~/src/services/autils');
 const QuestCoreV2 = require('~/src/harmony/contracts/questCoreV2');
 const questCoreV2Contract = new QuestCoreV2();
 const minStamina = 25;
+const maxQueue = 6;
 
 exports.CheckAndSendFishers = async (heroesStruct) => {
-	const questType = config.harmony.quest.fishing
-	const activeQuesterIds = heroesStruct.allQuesters
-	const heroObjects = await autils.getHerosInfo(questType.heroes)
-	const possibleFishers = heroObjects.filter((heroObject) => { return activeQuesterIds.indexOf(heroObject.id) === -1 && heroObject.currentStamina >= minStamina && heroObject.owner === config.walletAddress })
-
-	if (possibleFishers.length > 0) {
-		for (let i = 0; i < possibleFishers.length; i++) {
-			console.log(`senting ${possibleFishers[i].id} to fishing quest`);
-			const attemp = possibleFishers[i].profession === "fishing" ? Math.floor(possibleFishers[i].currentStamina / 5) : Math.floor(possibleFishers[i].currentStamina / 7)
-			await questCoreV2Contract.startFishingQuest([possibleFishers[i].id], attemp);
-		}
+	if (heroesStruct.fishingQuestCount >= maxQueue) {
+		console.log("fishing queue has reached its maximum value.")
 	} else {
-		console.log("No fisher sent")
+		const questType = config.harmony.quest.fishing
+		const activeQuesterIds = heroesStruct.allQuesters
+		const heroObjects = await autils.getHerosInfo(questType.heroes)
+		const possibleFishers = heroObjects.filter((heroObject) => { return activeQuesterIds.indexOf(heroObject.id) === -1 && heroObject.currentStamina >= minStamina && heroObject.owner === config.walletAddress })
+	
+		if (possibleFishers.length > 0) {
+			for (let i = 0; i < maxQueue - heroesStruct.fishingQuestCount - 1; i++) {
+				console.log(`senting ${possibleFishers[i].id} to fishing quest`);
+				const attemp = possibleFishers[i].profession === "fishing" ? Math.floor(possibleFishers[i].currentStamina / 5) : Math.floor(possibleFishers[i].currentStamina / 7)
+				await questCoreV2Contract.startFishingQuest([possibleFishers[i].id], attemp);
+			}
+		} else {
+			console.log("No fisher sent")
+		}
 	}
 }
