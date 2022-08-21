b04c3006d16c3e3da7cd4482cdb69e8f42a53651
Galen Lin <bag571ivy3470@gmail.com>
2022-08-21 12:40:46 +0800

    bug(script): fix harmony sale_handler logic mistake

--- src/harmony/sales_handler.js
+++ src/harmony/sales_handler.js
@@ -8,11 +8,11 @@ const isOwning = (heroObject) => {
 }
 
 const isShouldList = (heroObject) => {
-  return isOwning(heroObject) && !heroObject.isOnSale && !heroObject.isOnRent && heroObject.currentStamina < config.defikingdoms.listStamina
+  return isOwning(heroObject) && !heroObject.isOnSale && !heroObject.isOnRent && heroObject.currentStamina < config.harmony.listStamina
 }
 
 const isShouldUnList = (heroObject) => {
-  return heroObject.isOnSale && heroObject.currentStamina > config.defikingdoms.unlistStamina;
+  return heroObject.isOnSale && heroObject.currentStamina > config.harmony.unlistStamina;
 }
 
 exports.runSalesLogic = async () => {
