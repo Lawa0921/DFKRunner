1ecc02981e79edccae0589d45a5737a5c21dd681
Galen Lin <bag571ivy3470@gmail.com>
2022-08-22 16:06:34 +0800

    chore(script): add gold mining quest logic

--- src/defikingdoms/quest_gold_mining.js
+++ src/defikingdoms/quest_gold_mining.js
@@ -3,15 +3,17 @@ const autils = require('~/src/services/autils');
 const QuestCoreV2 = require('~/src/defikingdoms/contracts/questCoreV2');
 const questCoreV2Contract = new QuestCoreV2();
 const minStamina = 25;
+const maxBatch = 6;
 
 exports.CheckAndSendDFKGoldMiners = async (heroesStruct) => {
   const questType = config.defikingdoms.quest.goldMining
   const activeQuesterIds = heroesStruct.allQuesters
   const heroObjects = await autils.getHerosInfo(questType.heroes)
   const possibleGoldMiners = heroObjects.filter((heroObject) => { return activeQuesterIds.indexOf(heroObject.id) === -1 && heroObject.currentStamina >= minStamina && heroObject.owner === config.walletAddress })
+  const batchAmount = questType.singleBatchAmount > maxBatch ? maxBatch : questType.singleBatchAmount
 
-  if (possibleGoldMiners.length > 0 && possibleGoldMiners.length >= questType.singleBatchAmount) {
-    const sentMinerIds = possibleGoldMiners.slice(0, questType.singleBatchAmount).map(heroObject => heroObject.id)
+  if (possibleGoldMiners.length > 0 && possibleGoldMiners.length >= batchAmount) {
+    const sentMinerIds = possibleGoldMiners.slice(0, batchAmount).map(heroObject => heroObject.id)
     console.log(`senting ${sentMinerIds} to gold mining quest`)
     await questCoreV2Contract.startGoldMining(sentMinerIds)
   } else {
--- src/harmony/quest_gold_mining.js
+++ src/harmony/quest_gold_mining.js
@@ -3,15 +3,17 @@ const autils = require('~/src/services/autils');
 const QuestCoreV1 = require('~/src/harmony/contracts/questCoreV1');
 const questCoreV1Contract = new QuestCoreV1();
 const minStamina = 25;
+const maxBatch = 6;
 
 exports.CheckAndSendGoldMiners = async (heroesStruct) => {
   const questType = config.harmony.quest.goldMining
   const activeQuesterIds = heroesStruct.allQuesters
   const heroObjects = await autils.getHerosInfo(questType.heroes)
   const possibleGoldMiners = heroObjects.filter((heroObject) => { return activeQuesterIds.indexOf(heroObject.id) === -1 && heroObject.currentStamina >= minStamina && heroObject.owner === config.walletAddress })
+  const batchAmount = questType.singleBatchAmount > maxBatch ? maxBatch : questType.singleBatchAmount
 
-  if (possibleGoldMiners.length > 0 && possibleGoldMiners.length >= questType.singleBatchAmount) {
-    const sentMinerIds = possibleGoldMiners.slice(0, questType.singleBatchAmount).map(heroObject => heroObject.id)
+  if (possibleGoldMiners.length > 0 && possibleGoldMiners.length >= batchAmount) {
+    const sentMinerIds = possibleGoldMiners.slice(0, batchAmount).map(heroObject => heroObject.id)
     console.log(`senting ${sentMinerIds} to gold mining quest`)
     await questCoreV1Contract.startGoldMining(sentMinerIds)
   } else {
