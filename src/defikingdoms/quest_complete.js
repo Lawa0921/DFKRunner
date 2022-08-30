b2159cbb012d1d28091365770a2df571bfef5504
Galen Lin <bag571ivy3470@gmail.com>
2022-08-30 22:19:17 +0800

    chore(script): adjust complete quest logic

--- src/defikingdoms/quest_complete.js
+++ src/defikingdoms/quest_complete.js
@@ -1,21 +1,27 @@
 const QuestCoreV2 = require('~/src/defikingdoms/contracts/questCoreV2')
-const autils = require('~/src/services/autils')
+const questCoreV2Contract = new QuestCoreV2();
 
 exports.CompleteQuests = async (heroesStruct) => {
   if (heroesStruct.completedQuesters.length > 0) {
-    const questCoreV2Contract = new QuestCoreV2();
+    let completeQuestPromises = heroesStruct.completedQuesters.map((completedQuesterId) => {
+      return completeQuest(completedQuesterId)
+    })
 
-    for (let index = 0; index < heroesStruct.completedQuesters.length; index++) {
-      const tx = await questCoreV2Contract.completeQuest(heroesStruct.completedQuesters[index])
-      const res = await tx.wait();
-  
-      if (res.status === 1) {
-        console.log(heroesStruct.completedQuesters[index] + " Quest completed")
-      } else {
-        autils.txnFailLog(heroesStruct.completedQuesters[index] + " Quest complete failed");
-      }
-    };
+    await Promise.allSettled(completeQuestPromises).then((results) => {
+      console.log(`completed ${results.length} quest`)
+    })
   } else {
     console.log("No quest should be complete.")
+  }
+}
+
+completeQuest = async (completedQuesterId) => {
+  const tx = await questCoreV2Contract.completeQuest(completedQuesterId)
+  const res = await tx.wait();
+
+  if (res.status === 1) {
+    console.log(completedQuesterId + " Quest completed")
+  } else {
+    console.log(completedQuesterId + " Quest complete failed");
   }
 }
