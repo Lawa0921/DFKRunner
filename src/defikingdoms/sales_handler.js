c419037c8da77a9ce707bf87b5df26a1f8c0f21e
Galen Lin <bag571ivy3470@gmail.com>
2022-09-01 20:16:39 +0800

    chore(script): add promise logic to sale_handler

--- src/defikingdoms/sales_handler.js
+++ src/defikingdoms/sales_handler.js
@@ -18,12 +18,23 @@ const isShouldUnList = (heroObject) => {
 exports.runDFKSalesLogic = async () => {
   const heroList = config.defikingdoms.heroForSale.sort((a, b) => { return parseInt(a.id) - parseInt(b.id); });
   const heroObjects = await autils.getHerosInfo(heroList.map((heroData) => { return heroData.id }))
+  let saleHandlerPromises = []
 
   for (let i = 0; i < heroObjects.length; i++) {
     if (isShouldUnList(heroObjects[i])) {
-      await saleAuctionContract.unlistHero(heroList[i].id);
+      saleHandlerPromises.push(unsellHero(heroList[i].id))
     } else if (isShouldList(heroObjects[i])) {
-      await saleAuctionContract.listHero(heroList[i].id, heroList[i].price);
+      saleHandlerPromises.push(sellHero(heroList[i].id, heroList[i].price))
     }
   }
+
+  await Promise.allSettled(saleHandlerPromises)
+}
+
+sellHero = async (heroId, price) => {
+  await saleAuctionContract.listHero(heroId, price);
+}
+
+unsellHero = async (heroId) => {
+  await saleAuctionContract.unlistHero(heroId);
 }
