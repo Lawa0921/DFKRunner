affa67870f85526e785523083876bdf196656d00
Galen Lin <bag571ivy3470@gmail.com>
2022-09-03 18:26:14 +0800

    chore(script): adjust stat_quest pattern

--- src/defikingdoms/quest_stats.js
+++ src/defikingdoms/quest_stats.js
@@ -5,21 +5,21 @@ const questCoreV2Contract = new QuestCoreV2();
 const minStamina = 25;
 
 exports.CheckAndSendDFKStatQuests = async (heroesStruct) => {
-    for (let i = 0; i < config.defikingdoms.quest.statQuests.length; i++) {
-        const questType = config.defikingdoms.quest.statQuests[i]
+  for (let i = 0; i < config.defikingdoms.quest.statQuests.length; i++) {
+  	const questType = config.defikingdoms.quest.statQuests[i]
 
-        if (questType.heroes.length !== 0) {
-            const activeQuesterIds = heroesStruct.allQuesters
-            const heroObjects = await autils.getHerosInfo(questType.heroes)
-            const possibleHeroes = heroObjects.filter((heroObject) => { return activeQuesterIds.indexOf(heroObject.id) === -1 && heroObject.currentStamina() >= minStamina && heroObject.owner === config.walletAddress })
+    if (questType.heroes.length !== 0) {
+			const activeQuesterIds = heroesStruct.allQuesters
+			const heroObjects = await autils.getHerosInfo(questType.heroes)
+			const possibleHeroes = heroObjects.filter((heroObject) => { return activeQuesterIds.indexOf(heroObject.id) === -1 && heroObject.currentStamina() >= minStamina && heroObject.owner === config.walletAddress })
     
-            if (possibleHeroes.length > 0) {
-                for (let i = 0; i < possibleHeroes.length; i++) {
-                    console.log(`senting ${possibleHeroes[i].id} to ${questType.name}`);
-                    const attemp = Math.floor(possibleHeroes[i].currentStamina() / 5)
-                    await questCoreV2Contract.startStatQuest([possibleHeroes[i].id], attemp, questType.contractAddress, questType.name);
-                }
-            }
-        }
-    }
+			if (possibleHeroes.length > 0) {
+				for (let i = 0; i < possibleHeroes.length; i++) {
+					console.log(`senting ${possibleHeroes[i].id} to ${questType.name}`);
+					const attemp = Math.floor(possibleHeroes[i].currentStamina() / 5)
+					await questCoreV2Contract.startStatQuest([possibleHeroes[i].id], attemp, questType.contractAddress, questType.name);
+				}
+			}
+		}
+	}
 }
