6c8a529d048d9e5710ef820e00c527da5d28ca3e
Galen Lin <bag571ivy3470@gmail.com>
2022-09-16 17:27:50 +0800

    chore(script): adjust send gardening quest logic

--- src/defikingdoms/quest_gardening.js
+++ src/defikingdoms/quest_gardening.js
@@ -1,6 +1,8 @@
 const config = require("~/config.js");
 const QuestCoreV2 = require('~/src/defikingdoms/contracts/questCoreV2');
 const questCoreV2Contract = new QuestCoreV2();
+const SaleAuction = require('~/src/defikingdoms/contracts/saleAuction');
+const saleAuctionContract = new SaleAuction();
 const minStamina = 25;
 
 exports.CheckAndSendDFKGardeners = async (heroesStruct, owningHeroObjects) => {
@@ -13,7 +15,15 @@ exports.CheckAndSendDFKGardeners = async (heroesStruct, owningHeroObjects) => {
     })
 
     if (possibleGardeners.length > 0 && possibleGardeners.length >= questType.pairAddressMappings[i].singleBatchAmount) {
-      const sentGardenerIds = possibleGardeners.slice(0, questType.pairAddressMappings[i].singleBatchAmount).map(heroObject => heroObject.id)
+      const sendGardeners = possibleGardeners.slice(0, questType.pairAddressMappings[i].singleBatchAmount)
+      const sentGardenerIds = sendGardeners.map(heroObject => heroObject.id)
+
+      for (let i = 0; i < i < sendGardeners.length; i++) {
+        if (sendGardeners[i].isOnSale) {
+          await saleAuctionContract.unlistHero(sendGardeners[i].id)
+        }
+      }
+
       console.log(`senting ${sentGardenerIds} to ${questType.pairAddressMappings[i].tokenPair} gardening quest`)
       await questCoreV2Contract.startGardeningQuest(sentGardenerIds, questType.pairAddressMappings[i].pairAddress)
     } else {
