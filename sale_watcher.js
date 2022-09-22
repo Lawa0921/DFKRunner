38b2f3c36b6e29b5231031c0780bc4633b5c4def
Galen Lin <bag571ivy3470@gmail.com>
2022-09-22 14:50:17 +0800

    chore(script): remove unused harmony sale_handler logic

--- sale_watcher.js
+++ sale_watcher.js
@@ -4,28 +4,9 @@ const axios = require('axios')
 const axiosRetry = require('axios-retry');
 const saleAuctionABI = require('~/abis/SaleAuction.json')
 const date = require('date-and-time');
-
-const { Wallet } = require('@harmony-js/account');
-const { Contract } = require("@harmony-js/contract")
-const { Messenger, WSProvider } = require('@harmony-js/network');
-const { ChainID, ChainType, hexToNumber } = require('@harmony-js/utils');
 const Valuator = require('~/src/services/valuator');
-const { bidHero } = require('~/src/harmony/tavern_bid');
-const ws = new WSProvider(config.harmony.webSocketsRpcs[0]);
 const DFKSaleAuction = require("~/src/defikingdoms/contracts/saleAuction");
 const DFKSaleAuctionContract = new DFKSaleAuction();
-
-const wallet = new Wallet(
-  new Messenger(
-    ws,
-    ChainType.Harmony,
-    ChainID.HmyMainnet,
-  ),
-);
-
-wallet.addByPrivateKey(config.privateKey);
-
-const tavernContract = new Contract(saleAuctionABI, config.harmony.saleAuction, {}, wallet)
 
 axiosRetry(axios, {
   retries: 5,
@@ -38,24 +19,11 @@ axiosRetry(axios, {
   },
 });
 
-const harmonySaleHandler = async (tokenId, price) => {
-  let heroObjects = await autils.getHerosInfo([tokenId]);
-
-  const valuator = new Valuator(price, heroObjects[0]);
-  await valuator.execute();
-
-  if (!valuator.hero.isOwning() && valuator.price <= valuator.valuation) {
-    await bidHero(tokenId, price);
-  }
-
-  autils.watchHeroLog(valuator.hero, price, valuator.valuation, "hmy");
-}
-
 const DFKSaleHandler = async (tokenId, price) => {
   let heroObjects = await autils.getHerosInfo([parseInt(tokenId)]);
 
   const valuator = new Valuator(price, heroObjects[0]);
-  await valuator.execute();
+  valuator.execute();
 
   if (!valuator.hero.isOwning() && valuator.price <= valuator.valuation) {
     console.log(`!!! bid hero ${tokenId} txn created !!!`)
@@ -81,18 +49,6 @@ async function main() {
     console.log(`${date.addMinutes(new Date(Date.now()), 0)}: restart process`)
     process.exit(0);
   }, 120000);
-
-  tavernContract.events.AuctionCreated().onData((event) => {
-    clearTimeout(timerId);
-    const { tokenId, startingPrice } = event.returnValues;
-
-    harmonySaleHandler(tokenId, startingPrice);
-
-    timerId = setTimeout(() => {
-      console.log(`${date.addMinutes(new Date(Date.now()), 0)}: restart process`)
-      process.exit(0);
-    }, 120000);
-  });
 
   DFKSaleAuctionContract.contract.on("AuctionCreated", (auctionId, owner, tokenId, startingPrice, endingPrice, duration, winner, event) => {
     clearTimeout(timerId);
