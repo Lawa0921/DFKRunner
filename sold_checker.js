f60b0ba2156b45dfcfaa3ddd3c45cd1525665700
Galen Lin <bag571ivy3470@gmail.com>
2022-09-29 00:44:41 +0800

    chore(script): fix sold_checker logic

--- sold_checker.js
+++ sold_checker.js
@@ -5,9 +5,9 @@ async function main() {
   const onSaleHeroIds = config.defikingdoms.heroForSale.map((heroData) => { return heroData.id });
   const heroObjects = await autils.getHerosInfo(onSaleHeroIds);
 
-  for (let index = 0; heroObjects.length > index; index++) {
-    if (heroObjects[index].owner !== config.walletAddress) {
-      console.log(`Hero ${heroObjects[index].id} is sold!`)
+  for (let i = 0; heroObjects.length > i; i++) {
+    if (config.walletAddressAndPrivateKeyMappings.map(accountInfo => accountInfo.walletAddress).indexOf(heroObjects[i].owner) === -1) {
+      console.log(`Hero ${heroObjects[i].id} is sold!`)
     }
   }
   console.log("process completed")
