186de47842e6279e5468f941403d557b44dba322
Lawa0921 <bag571ivy3470@gmail.com>
2022-11-12 17:56:20 +0800

    bug(script): fix auto dueler logic bug

--- auto_dueler.js
+++ auto_dueler.js
@@ -3,6 +3,7 @@ const autils = require('~/src/services/autils');
 const DFKDuelS1 = require('~/src/defikingdoms/contracts/DFKDuelS1')
 
 main = async() => {
+	console.log(autils.getCurrentDateTime().toLocaleTimeString());
 	const autoDuelPromise = config.walletAddressAndPrivateKeyMappings.map((accountInfo) => {
 		autoDuelScript(accountInfo)
 	})
@@ -17,7 +18,7 @@ autoDuelScript = async (accountInfo) => {
   try {
 		const DFKDuelSetting = config.defikingdoms.duelSetting.filter(duelSetting => duelSetting.isActive && duelSetting.heroes.length > 0)
 		
-		for (let i = 0; i <= DFKDuelSetting.length; i++) {
+		for (let i = 0; i < DFKDuelSetting.length; i++) {
 			const DFKDuelS1Contract = new DFKDuelS1(accountInfo)
 			const activeDuels = await DFKDuelS1Contract.getActiveDuels().then(res => res.filter(duel => duel.duelType === DFKDuelS1Contract.duelType()[DFKDuelSetting[i].type]))
 			const activeEntries = await DFKDuelS1Contract.getPlayerDuelEntries().then(res => res.filter(entry => entry.duelType === DFKDuelS1Contract.duelType()[DFKDuelSetting[i].type]))
@@ -28,7 +29,7 @@ autoDuelScript = async (accountInfo) => {
 				await DFKDuelS1Contract.matchMake(DFKDuelSetting[i].type)
 			} else {
 				const duelerHeroes = await Promise.allSettled(DFKDuelSetting[i].heroes.map(heroIds => autils.getHeroesInfoByIds(heroIds))).then((res) => res.map(promiseResult => promiseResult.value))
-				const duelHistory = await DFKDuelS1Contract.getDuelHistory().filter(duelHistory => duelHistory.duelType === DFKDuelS1Contract.duelType()[DFKDuelSetting[i].type])
+				const duelHistory = await DFKDuelS1Contract.getDuelHistory().then(res => res.filter(duelHistory => duelHistory.duelType === DFKDuelS1Contract.duelType()[DFKDuelSetting[i].type]))
 				
 				// to do
 			}
@@ -36,6 +37,7 @@ autoDuelScript = async (accountInfo) => {
 		
     console.log(`--- ${accountInfo.accountName} auto dueler process completed ---`)
   } catch(error) {
+		console.log(error)
     process.exit();
   }
 }
