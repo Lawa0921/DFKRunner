f233b744f43f3d3d3e39308878110ba5c6438dda
Lawa0921 <bag571ivy3470@gmail.com>
2022-11-12 17:42:06 +0800

    feat(script): create autoDueler basic logic

--- auto_dueler.js
+++ auto_dueler.js
@@ -1,8 +1,43 @@
 const config = require("~/config.js");
 const autils = require('~/src/services/autils');
+const DFKDuelS1 = require('~/src/defikingdoms/contracts/DFKDuelS1')
 
 main = async() => {
-    // to do
+	const autoDuelPromise = config.walletAddressAndPrivateKeyMappings.map((accountInfo) => {
+		autoDuelScript(accountInfo)
+	})
+	
+	await Promise.allSettled(autoDuelPromise)
+  await autils.sleep(config.setDuelScriptTimeSecond * 1000)
+
+  process.exit()
+}
+
+autoDuelScript = async (accountInfo) => {
+  try {
+		const DFKDuelSetting = config.defikingdoms.duelSetting.filter(duelSetting => duelSetting.isActive && duelSetting.heroes.length > 0)
+		
+		for (let i = 0; i <= DFKDuelSetting.length; i++) {
+			const DFKDuelS1Contract = new DFKDuelS1(accountInfo)
+			const activeDuels = await DFKDuelS1Contract.getActiveDuels().then(res => res.filter(duel => duel.duelType === DFKDuelS1Contract.duelType()[DFKDuelSetting[i].type]))
+			const activeEntries = await DFKDuelS1Contract.getPlayerDuelEntries().then(res => res.filter(entry => entry.duelType === DFKDuelS1Contract.duelType()[DFKDuelSetting[i].type]))
+			
+			if (activeDuels.length > 0) {
+				await Promise.allSettled(activeDuels.map(duel => DFKDuelS1Contract.completeDuel((duel.id))))
+			} else if (activeEntries.length > 0) {
+				await DFKDuelS1Contract.matchMake(DFKDuelSetting[i].type)
+			} else {
+				const duelerHeroes = await Promise.allSettled(DFKDuelSetting[i].heroes.map(heroIds => autils.getHeroesInfoByIds(heroIds))).then((res) => res.map(promiseResult => promiseResult.value))
+				const duelHistory = await DFKDuelS1Contract.getDuelHistory().filter(duelHistory => duelHistory.duelType === DFKDuelS1Contract.duelType()[DFKDuelSetting[i].type])
+				
+				// to do
+			}
+		}
+		
+    console.log(`--- ${accountInfo.accountName} auto dueler process completed ---`)
+  } catch(error) {
+    process.exit();
+  }
 }
 
 main()
--- src/defikingdoms/contracts/DFKDuelS1.js
+++ src/defikingdoms/contracts/DFKDuelS1.js
@@ -4,52 +4,6 @@ const ethers = require('ethers');
 const DFKDuelS1ABI = require('~/abis/DFKDuelS1.json')
 const { NonceManager } = require("@ethersproject/experimental")
 
-const type = {
-	'solo': 1,
-	'squad': 3,
-	'war': 9
-}
-
-const background = {
-	'desert': 0,
-	'forest': 2,
-	'plains': 4,
-	'island': 6,
-	'swamp': 8,
-	'mountains': 10,
-	'city': 12,
-	'arctic': 14
-}
-
-const stat = {
-	'STR': 0,
-	'AGI': 2,
-	'INT': 4,
-	'WIS': 6,
-	'LCK': 8,
-	'VIT': 10,
-	'END': 12,
-	'DEX': 14
-}
-
-const typeFee = {
-	"solo": {
-		"low": "0.1",
-		"medium": "0.5",
-		"large": "1"
-	},
-	"squad": {
-		"low": "0.3",
-		"medium": "1.5",
-		"large": "3"
-	},
-	"war": {
-		"low": "1",
-		"medium": "5",
-		"large": "10"
-	}
-}
-
 module.exports = class DFKDuelS1Contract {
   constructor(accountInfo) {
 		this.provider = new ethers.providers.JsonRpcProvider(config.defikingdoms.rpcs[config.defikingdoms.useRpcIndex])
@@ -60,11 +14,11 @@ module.exports = class DFKDuelS1Contract {
 
   async enterDuelLobby(duelType, heroIds, fee, duelBackground, duelStat) {
     const txn = await this.contract.enterDuelLobby(
-			type[duelType],
+			this.duelType()[duelType],
 			heroIds,
-			ethers.utils.parseEther(typeFee[duelType][fee], "ether"),
-			background[duelBackground],
-			stat[duelStat],
+			ethers.utils.parseEther(this.duelFee()[duelType][fee], "ether"),
+			this.duelBackground()[duelBackground],
+			this.duelStat()[duelStat],
 			{ gasPrice: await autils.getBaseGasFee() }
 		)
 
@@ -153,7 +107,7 @@ module.exports = class DFKDuelS1Contract {
 	}
 
 	async matchMake(type) {
-		const txn = await this.contract.matchMake(type)
+		const txn = await this.contract.matchMake(this.duelType()[type])
 		const receipt = await txn.wait()
 
 		if (receipt.status === 1) {
@@ -162,6 +116,68 @@ module.exports = class DFKDuelS1Contract {
       console.log(`match ${type} duel failed`)
     }
     return receipt
+	}
+
+	getHeroesBestStat(heroObjects) {
+		// to do
+	}
+
+	getHeroesBestBackground(heroObjects) {
+		// to do
+	}
+
+	duelType() {
+		return {
+			'solo': 1,
+			'squad': 3,
+			'war': 9
+		}
+	}
+
+	duelBackground() {
+		return {
+			'desert': 0,
+			'forest': 2,
+			'plains': 4,
+			'island': 6,
+			'swamp': 8,
+			'mountains': 10,
+			'city': 12,
+			'arctic': 14
+		}
+	}
+
+	duelStat() {
+		return {
+			'STR': 0,
+			'AGI': 2,
+			'INT': 4,
+			'WIS': 6,
+			'LCK': 8,
+			'VIT': 10,
+			'END': 12,
+			'DEX': 14
+		}
+	}
+
+	duelFee() {
+		return {
+			"solo": {
+				"low": "0.1",
+				"medium": "0.5",
+				"large": "1"
+			},
+			"squad": {
+				"low": "0.3",
+				"medium": "1.5",
+				"large": "3"
+			},
+			"war": {
+				"low": "1",
+				"medium": "5",
+				"large": "10"
+			}
+		}
 	}
 }
 
