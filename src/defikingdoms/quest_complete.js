41358e0693f729d141eb35d73c01a34df0e7bf68
Lawa0921 <bag571ivy3470@gmail.com>
2022-12-08 16:34:54 +0800

    chore(script): adjust dfk complete quest logic

--- src/defikingdoms/quest_complete.js
+++ src/defikingdoms/quest_complete.js
@@ -10,18 +10,18 @@ exports.CompleteQuests = async (heroesStruct, accountInfo) => {
 
     await Promise.allSettled(completeQuestPromises)
   } else {
-    console.log(`${accountInfo.accountName} No quest should be complete.`)
+    console.log(`${accountInfo.accountName} DFK No quest should be complete.`)
   }
 }
 
 completeQuest = async (completedQuesterId, questCoreV2Contract, accountName) => {
-  console.log(`${accountName} sending complete ${completedQuesterId} quest`)
+  console.log(`${accountName} DFK sending complete ${completedQuesterId} quest`)
   const tx = await questCoreV2Contract.completeQuest(completedQuesterId)
   const res = await tx.wait();
 
   if (res.status === 1) {
-    console.log(`${accountName} ${completedQuesterId} Quest completed`)
+    console.log(`${accountName} DFK ${completedQuesterId} Quest completed`)
   } else {
-    console.log(`${accountName} ${completedQuesterId} Quest complete failed`);
+    console.log(`${accountName} DFK ${completedQuesterId} Quest complete failed`);
   }
 }
