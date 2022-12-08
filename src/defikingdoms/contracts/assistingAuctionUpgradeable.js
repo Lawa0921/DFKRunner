04a1c88edaab8b98fa9748cefe9a6c9ee52ac3a5
Lawa0921 <bag571ivy3470@gmail.com>
2022-12-08 20:09:30 +0800

    chore(contract): create assistingAuctionUpgradable contract logic

---
+++ src/defikingdoms/contracts/assistingAuctionUpgradeable.js
@@ -0,0 +1,53 @@
+const config = require("~/config.js");
+const ethers = require('ethers');
+const assistingAuctionUpgradeableABI = require('~/abis/AssistingAuctionUpgradeable.json');
+const autils = require('~/src/services/autils');
+const { NonceManager } = require("@ethersproject/experimental")
+
+module.exports = class AssistingAuctionUpgradeable {
+  constructor(accountInfo) {
+    this.provider = new ethers.providers.JsonRpcProvider(config.defikingdoms.rpcs[config.defikingdoms.useRpcIndex])
+    this.wallet = new ethers.Wallet(accountInfo.privateKey, this.provider)
+    this.contract = new ethers.Contract(config.defikingdoms.assistingAuctionUpgradable, assistingAuctionUpgradeableABI, new NonceManager(this.wallet))
+    this.accountName = accountInfo.accountName
+  }
+
+  async cancelAuction(heroId) {
+    return await this.contract.cancelAuction(heroId, { gasPrice: await autils.getDFKGasFee() })
+  }
+
+  async createAuction(heroId, price) {
+    return await this.contract.createAuction(heroId, autils.ethersFormatNumberToWei(price), autils.ethersFormatNumberToWei(price), 60, autils.get0xAddress(), { gasPrice: await autils.getDFKGasFee() });
+  }
+
+  async listHero(heroId, price) {
+    const id = parseInt(heroId, 10);
+    console.log(`${this.accountName} DFK renting hero: ${id}: ${price} C`);
+  
+    const tx = await this.createAuction(id, price)
+    const res = await tx.wait();
+
+    if (res.status === 1) {
+      console.log(`${this.accountName} DFK rented hero: ${id} completed`);
+    } else {
+      console.log(`${this.accountName} DFK rented hero: ${id} failed`);
+    }
+
+    return res;
+  }
+
+  async unlistHero(heroId) {
+    const id = parseInt(heroId, 10);
+    console.log(`${this.accountName} DFK unrenting hero: ${id}`);
+
+    const tx = await this.cancelAuction(id);
+    const res = await tx.wait();
+    if (res.status === 1) {
+      console.log(`${this.accountName} DFK unrented hero: ${id} completed`);
+    } else {
+      console.log(`${this.accountName} DFK unrented hero: ${id} failed`);
+    }
+
+    return res;
+  }
+}
---
+++ src/klay/contracts/assistingAuctionUpgradeable.js
@@ -0,0 +1,53 @@
+const config = require("~/config.js");
+const ethers = require('ethers');
+const assistingAuctionUpgradeableABI = require('~/abis/AssistingAuctionUpgradeable.json');
+const autils = require('~/src/services/autils');
+const { NonceManager } = require("@ethersproject/experimental")
+
+module.exports = class AssistingAuctionUpgradeable {
+  constructor(accountInfo) {
+    this.provider = new ethers.providers.JsonRpcProvider(config.klay.rpcs[config.klay.useRpcIndex])
+    this.wallet = new ethers.Wallet(accountInfo.privateKey, this.provider)
+    this.contract = new ethers.Contract(config.klay.assistingAuctionUpgradable, assistingAuctionUpgradeableABI, new NonceManager(this.wallet))
+    this.accountName = accountInfo.accountName
+  }
+
+  async cancelAuction(heroId) {
+    return await this.contract.cancelAuction(heroId, { gasPrice: await autils.getKLAYGasFee() })
+  }
+
+  async createAuction(heroId, price) {
+    return await this.contract.createAuction(heroId, autils.ethersFormatNumberToWei(price), autils.ethersFormatNumberToWei(price), 60, autils.get0xAddress(), { gasPrice: await autils.getKLAYGasFee() });
+  }
+
+  async listHero(heroId, price) {
+    const id = parseInt(heroId, 10);
+    console.log(`${this.accountName} DFK renting hero: ${id}: ${price} Jade`);
+  
+    const tx = await this.createAuction(id, price)
+    const res = await tx.wait();
+
+    if (res.status === 1) {
+      console.log(`${this.accountName} DFK rented hero: ${id} completed`);
+    } else {
+      console.log(`${this.accountName} DFK rented hero: ${id} failed`);
+    }
+
+    return res;
+  }
+
+  async unlistHero(heroId) {
+    const id = parseInt(heroId, 10);
+    console.log(`${this.accountName} DFK unrenting hero: ${id}`);
+
+    const tx = await this.cancelAuction(id);
+    const res = await tx.wait();
+    if (res.status === 1) {
+      console.log(`${this.accountName} DFK unrented hero: ${id} completed`);
+    } else {
+      console.log(`${this.accountName} DFK unrented hero: ${id} failed`);
+    }
+
+    return res;
+  }
+}
