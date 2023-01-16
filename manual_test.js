3fa90e1857110f9c6502c94e1b9f7829cf2e3b44
Lawa0921 <bag571ivy3470@gmail.com>
2023-01-16 10:16:27 +0800

    chore(config): update config

--- manual_test.js
+++ manual_test.js
@@ -4,6 +4,7 @@ const autils = require('./src/services/autils');
 const ethers = require('ethers');
 const HeroCore = require("./src/defikingdoms/contracts/heroCore");
 const KLAYQuestCore = require("./src/klay/contracts/questCoreV2");
+const DFKQuestCore = require("./src/defikingdoms/contracts/questCoreV2");
 const RaffleMaster = require("./src/defikingdoms/contracts/raffleMaster")
 const DuelRaffleTicket = require("./src/defikingdoms/contracts/duelRaffeTicket")
 const AirdropClaim = require("./src/defikingdoms/contracts/airdropClaim")
@@ -18,6 +19,7 @@ const crystalContract = new Crystal(config.walletAddressAndPrivateKeyMappings[0]
 const assistingAuctionUpgradeableContract = new AssistingAuctionUpgradeable(config.walletAddressAndPrivateKeyMappings[0])
 const DFKDuelS2Contract = new DFKDuelS2(config.walletAddressAndPrivateKeyMappings[0])
 const KLAYQuestCoreContract = new KLAYQuestCore(config.walletAddressAndPrivateKeyMappings[0])
+const DFKQuestCoreContract = new DFKQuestCore(config.walletAddressAndPrivateKeyMappings[0])
 const { enterRaffle } = require("./src/klay/enter_raffle");
 
 const Valuator = require('~/src/services/valuator');
@@ -27,7 +29,7 @@ const { airdropClaim } = require("./src/defikingdoms/airdrop_claim")
 async function test() {  
   // console.log(Math.floor(new Date().getTime() / 1000))
   // await enterRaffle(config.walletAddressAndPrivateKeyMappings[0]);
-  console.log(await KLAYQuestCoreContract.cancelQuest("1000000028544"))
+  console.log(await DFKQuestCoreContract.cancelQuest("162779"))
   // console.log(await assistingAuctionUpgradeableContract.userAuctions())
   // console.log(Math.round(8.5 * 100) / 100)
   // console.log(await crystalContract.allowance(config.defikingdoms.meditationCircle))
