a9114a911f04561c962edff496e8211e59192e05
Lawa0921 <bag571ivy3470@gmail.com>
2023-05-24 20:10:14 +0800

    Remove questCoreV2 related file

--- manual_test.js
+++ manual_test.js
@@ -1,16 +1,13 @@
 // 測試用
 const config = require("./config.js");
 const autils = require('./src/services/autils');
-const QuestCoreV2 = require('./src/defikingdoms/contracts/questCoreV2');
 const DFKAssistingAuctionUpgradeable = require("./src/defikingdoms/contracts/assistingAuctionUpgradeable")
 const KLAYAssistingAuctionUpgradeable = require("./src/klay/contracts/assistingAuctionUpgradeable")
 const DFKAssistingAuctionUpgradeableContract = new DFKAssistingAuctionUpgradeable(config.walletAddressAndPrivateKeyMappings[0])
 const KLAYAssistingAuctionUpgradeableContract = new KLAYAssistingAuctionUpgradeable(config.walletAddressAndPrivateKeyMappings[0])
-const DFKQuestCoreContract = new QuestCoreV2(config.walletAddressAndPrivateKeyMappings[0])
 
 async function test() {  
   try {
-    await DFKQuestCoreContract.cancelQuest("1000000125935")
   } catch(error) {
     console.log(`fail reason: ${error.reason}`)
     console.log(`error code: ${error.code}`)
